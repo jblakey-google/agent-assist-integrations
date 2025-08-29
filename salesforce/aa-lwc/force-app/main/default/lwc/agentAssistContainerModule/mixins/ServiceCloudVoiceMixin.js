12c7aa66cc3948157c334365659aa5f89091b4d3
Jordan Blakey <jblakey@google.com>
2025-08-29 22:21:31 +0000

    Implements retry logic and makes scv mixin configurable for nice telephony (so that other telephony platforms can be added easily).

--- salesforce/aa-lwc/force-app/main/default/lwc/agentAssistContainerModule/mixins/ServiceCloudVoiceMixin.js
+++ salesforce/aa-lwc/force-app/main/default/lwc/agentAssistContainerModule/mixins/ServiceCloudVoiceMixin.js
@@ -16,106 +16,29 @@
 
 import { wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
-// import CALLER_ID from "@salesforce/schema/VoiceCall.CallerId";
-// import CALLER_ID_TYPE from "@salesforce/schema/VoiceCall.CallerIdType";
-// import CONFERENCE_KEY from "@salesforce/schema/VoiceCall.ConferenceKey";
-// import CONVERSATION_ID from "@salesforce/schema/VoiceCall.ConversationId";
-// import ID from "@salesforce/schema/VoiceCall.Id";
-// import NAME from "@salesforce/schema/VoiceCall.Name";
-// import RELATED_RECORD_ID from "@salesforce/schema/VoiceCall.RelatedRecordId";
-// import VENDOR_CALL_KEY from "@salesforce/schema/VoiceCall.VendorCallKey";
-// import VENDOR_TYPE from "@salesforce/schema/VoiceCall.VendorType";
+import scvEventNames from "../data/scvEventNames";
 
-const FIELDS = [
-  "VoiceCall.CallType",
-  "VoiceCall.FromPhoneNumber",
-  "VoiceCall.ToPhoneNumber"
-  // CALLER_ID,
-  // CALLER_ID_TYPE,
-  // CONFERENCE_KEY,
-  // CONVERSATION_ID,
-  // ID,
-  // NAME,
-  // RELATED_RECORD_ID,
-  // VENDOR_CALL_KEY,
-  // VENDOR_TYPE
-];
-
-// great examples here
+// Great SCV LWC examples here
 // https://github.com/service-cloud-voice/examples-from-doc/blob/main/ToolkitAPI/sampleLWCComponent/sampleLWCComponent.js
+
+// VoiceCall Object reference
+// https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_voicecall.htm
+const FIELDS = ["VoiceCall.VendorCallKey"];
+const SCV_EVENTS_TO_SUBSCRIBE = [scvEventNames.callconnected];
+
+// SCV telephony config
+const CONFIG = {
+  // For this.platform = "servicecloud-voice", the Nice Business Unit Number
+  // https://help.nicecxone.com/content/acd/businessunits/managebusinessunit.htm
+  niceBusNo: "4610247" // TODO: make sure this matches your Nice CXone Business Unit Number.
+};
 
 const ServiceCloudVoiceMixin = (BaseClass) =>
   class extends BaseClass {
     @wire(getRecord, { recordId: "$recordId", fields: FIELDS }) voiceCall;
 
-    get CallType() {
-      return this.voiceCall.data.fields.CallType.value;
-    }
-
-    get FromPhoneNumber() {
-      return this.voiceCall.data.fields.FromPhoneNumber.value;
-    }
-
-    get ToPhoneNumber() {
-      return this.voiceCall.data.fields.ToPhoneNumber.value;
-    }
-
-    // get CallerId() {
-    //   return this.voiceCall.data.fields.CallerId.value;
-    // }
-
-    // get CallerIdType() {
-    //   return this.voiceCall.data.fields.CallerIdType.value;
-    // }
-
-    // get ConferenceKey() {
-    //   return this.voiceCall.data.fields.ConferenceKey.value;
-    // }
-
-    // get ConversationId() {
-    //   return this.voiceCall.data.fields.ConversationId.value;
-    // }
-
-    // get Id() {
-    //   return this.voiceCall.data.fields.Id.value;
-    // }
-
-    // get Name() {
-    //   return this.voiceCall.data.fields.Name.value;
-    // }
-
-    // get RelatedRecordId() {
-    //   return this.voiceCall.data.fields.RelatedRecordId.value;
-    // }
-
-    // get VendorCallKey() {
-    //   return this.voiceCall.data.fields.VendorCallKey.value;
-    // }
-
-    // get VendorType() {
-    //   return this.voiceCall.data.fields.VendorType.value;
-    // }
-
-    get options() {
-      return [
-        { label: "INITIAL_CALLER", value: "Initial_Caller" },
-        { label: "THIRD_PARTY", value: "Third_Party" }
-      ];
-    }
-
-    get endCallOptions() {
-      return [
-        { label: "AGENT", value: "Agent" },
-        { label: "INITIAL_CALLER", value: "Initial_Caller" },
-        { label: "THIRD_PARTY", value: "Third_Party" }
-      ];
-    }
-
-    get contactOptions() {
-      return [
-        { label: "PHONE NUMBER", value: "PhoneNumber" },
-        { label: "AGENT/QUEUE ID", value: "AgentOrQueueId" }
-      ];
+    get VendorCallKey() {
+      return this.voiceCall.data.fields.VendorCallKey.value;
     }
 
     constructor() {
@@ -130,19 +53,8 @@ const ServiceCloudVoiceMixin = (BaseClass) =>
     initServiceCloudVoice() {
       // Set up Agent Assist UIM to work with Service Cloud Voice.
       this.debugLog("initServiceCloudVoice called");
-      // this.debugLog(`CallType: ${this.CallType}`);
-      // this.debugLog(`FromPhoneNumber: ${this.FromPhoneNumber}`);
-      // this.debugLog(`ToPhoneNumber: ${this.ToPhoneNumber}`);
-      // this.debugLog(`CallerId: ${this.CallerId}`);
-      // this.debugLog(`CallerIdType: ${this.CallerIdType}`);
-      // this.debugLog(`ConferenceKey: ${this.ConferenceKey}`);
-      // this.debugLog(`ConversationId: ${this.ConversationId}`);
-      // this.debugLog(`Id: ${this.Id}`);
-      // this.debugLog(`Name: ${this.Name}`);
-      // this.debugLog(`RelatedRecordId: ${this.RelatedRecordId}`);
-      // this.debugLog(`VendorCallKey: ${this.VendorCallKey}`);
-      // this.debugLog(`VendorType: ${this.VendorType}`);
-      const toolkitApi = this.getToolkitApi();
+      this.debugLog(`this.VendorCallKey: ${this.VendorCallKey}`);
+      const toolkitApi = this.refs.serviceCloudVoiceToolkitApi;
       this.unsubscribeFromVoiceToolkit(toolkitApi, this.telephonyEventListener);
       this.subscribeToVoiceToolkit(toolkitApi, this.telephonyEventListener);
     }
@@ -157,13 +69,15 @@ const ServiceCloudVoiceMixin = (BaseClass) =>
     ////////////////////////////////////////////////////////////////////////////
 
     subscribeToVoiceToolkit(toolkitApi, telephonyEventListener) {
-      for (const eventName of this.scvEventNames) {
+      this.debugLog(`subscribeToVoiceToolkit: ${SCV_EVENTS_TO_SUBSCRIBE}`);
+      for (const eventName of SCV_EVENTS_TO_SUBSCRIBE) {
         toolkitApi.addEventListener(eventName, telephonyEventListener);
       }
     }
 
     unsubscribeFromVoiceToolkit(toolkitApi, telephonyEventListener) {
-      for (const eventName of this.scvEventNames) {
+      this.debugLog(`unsubscribeFromVoiceToolkit: ${SCV_EVENTS_TO_SUBSCRIBE}`);
+      for (const eventName of SCV_EVENTS_TO_SUBSCRIBE) {
         toolkitApi.addEventListener(eventName, telephonyEventListener);
       }
     }
@@ -172,69 +86,32 @@ const ServiceCloudVoiceMixin = (BaseClass) =>
     // Handle Events
     ////////////////////////////////////////////////////////////////////////////
 
-    scvEventNames = [
-      "audiostats",
-      "callstarted",
-      "callconnected",
-      "callended",
-      "hold",
-      "resume",
-      "mute",
-      "unmute",
-      "participantadded",
-      "participantremoved",
-      "conference",
-      "swap",
-      "pauserecording",
-      "resumerecording",
-      "transcript",
-      "wrapupended",
-      "flagraise",
-      "flaglower",
-      "note"
-    ];
-    payload = '{"key": "value"}';
-    teleEvent = "No events received yet.";
-    transcript = "No transcripts received yet.";
-    previewPhoneNumber = "";
-    addParticipantPhoneNumber = "";
-    sendDigits = "";
-    comboBoxHoldValue = "Initial_Caller";
-    comboBoxResumeValue = "Initial_Caller";
-    comboBoxRemoveParticipantValue = "Agent";
-    comboBoxContactTypeValue = "PhoneNumber";
-    comboBoxAddParticipantContactTypeValue = "PhoneNumber";
-
-    getToolkitApi() {
-      return this.refs.serviceCloudVoiceToolkitApi;
-    }
-
-    generateNiceConversationName(event) {
-      const niceBusNo = 4610247;
-      const callId = event.detail.callId;
-      const prefix = this.conversationProfile.split("/conversationProfiles")[0];
-      this.conversationId = `BusNo-${niceBusNo}_ContactId-${callId}`;
-      this.conversationName = `${prefix}/conversations/${this.conversationId}`;
-    }
-
     onTelephonyEvent(event) {
-      console.log(`[onTelephonyEvent] ${event.type}:`, event);
+      this.debugLog(
+        `[onTelephonyEvent ${event.type}]: ${JSON.stringify(event)}`
+      );
       if (event.type === "callconnected") {
-        this.generateNiceConversationName(event);
+        // Compare the SCV telephony event's callId to SF VoiceCall record's VendorCallKey.
+        // This is most likely also the BYOT telephony platform's external unique call id,
+        // which can be used to construct telephony platform specific DF conversationName.
+        this.debugLog(
+          `this.VendorCallKey: ${this.VendorCallKey}, event.detail.callId: ${event.detail.callId}`
+        );
+        if (this.VendorCallKey === event.detail.callId) {
+          if (this.scvTelephonyCheck.isNice) {
+            this.generateNiceConversationName(event.detail.callId);
+          } else {
+            this.debugLog("Unsupported SCV telephony platform.");
+          }
+        }
       }
-      if (
-        (event.type === "callstarted" || event.type === "callconnected") &&
-        this.comboBoxRemoveParticipantValue === "Agent"
-      ) {
-        this.isTelephonyActionControlsDisabled = false;
-      }
-      if (event.type === "callended") {
-        this.isTelephonyActionControlsDisabled = true;
-      }
-      if (event.type === "transcript") {
-        this.transcript = JSON.stringify(event.detail);
-      }
-      this.teleEvent = JSON.stringify(event);
+    }
+
+    generateNiceConversationName(callId) {
+      // Generate Nice CXone (Agent Assist Hub) formatted conversationName.
+      const prefix = this.conversationProfile.split("/conversationProfiles")[0];
+      this.conversationId = `BusNo-${CONFIG.niceBusNo}_ContactId-${callId}`;
+      this.conversationName = `${prefix}/conversations/${this.conversationId}`;
     }
   };
 
